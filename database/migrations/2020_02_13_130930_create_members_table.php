b5df3d0d951f5d1adc994a8f625664c49a2d691f
TheOnlySmartBoy <o2jose43@gmail.com>
2020-02-13 16:23:57 +0300

    Added Migration files

---
+++ database/migrations/2020_02_13_130930_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->string('tel');
+            $table->tinyInteger('ministry')->nullable();
+            $table->tinyInteger('role')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}

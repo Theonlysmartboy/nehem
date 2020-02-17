7bd95180955d768b13b9f1cf449b317bc2d97250
TheOnlySmartBoy <o2jose43@gmail.com>
2020-02-17 12:05:56 +0300

    Added user layouts design and routes

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::prefix('admin')->group(function(){
   Route::post('/login', 'AdminController@login')->name('admin.login.submit');
   Route::get('/', 'DashboardController@index')->name('admin.dashboard');
+  Route::get('/view/user', 'UsersController@index')->name('admin.view.user');
+  Route::get('/add/user', 'UsersController@showAddUserForm')->name('admin.add.user');
+  Route::post('/add/user', 'UsersController@create')->name('admin.add.user.submit');
+  Route::get('/edit/user', 'UsersController@showEditUserForm')->name('admin.edit.user');
+  Route::post('/edit/user', 'UsersController@update')->name('admin.edit.user.post');
 });

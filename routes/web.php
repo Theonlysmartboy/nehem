90b9a7209006edcd694d9650c372a60be6ee4aed
TheOnlySmartBoy <o2jose43@gmail.com>
2020-02-17 19:35:07 +0300

    added member routes

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,12 @@ Route::prefix('admin')->group(function(){
   Route::get('/edit/user', 'UsersController@showEditUserForm')->name('admin.edit.user');
   Route::post('/edit/user', 'UsersController@update')->name('admin.edit.user.post');
 });
+Route::prefix('user')->group(function(){
+  Route::post('/login', 'AdminController@login')->name('user.login.submit');
+  Route::get('/', 'DashboardController@index')->name('user.dashboard');
+  Route::get('/view/member', 'MembersController@index')->name('user.view.member');
+  Route::get('/add/member', 'MembersController@showAddMemberForm')->name('admin.add.user');
+  Route::post('/add/member', 'MembersController@create')->name('admin.add.user.submit');
+  Route::get('/edit/user', 'MembersController@showEditMemberForm')->name('admin.edit.user');
+  Route::post('/edit/user', 'MembersController@update')->name('admin.edit.user.post');
+});

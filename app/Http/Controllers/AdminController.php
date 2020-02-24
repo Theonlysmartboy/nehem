a7286969541abe1bb6bacf789d6122c5ddfcd018
TheOnlySmartBoy <o2jose43@gmail.com>
2020-02-24 08:57:51 +0300

    Updated the login logic to include remember functionality

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,16 +20,20 @@ class AdminController extends Controller
    }
   }
     public function login(Request $request){
-       $data = $request->input();
+      //validate form data
+      $this->validate($request,[
+        'email'=>'required|email',
+        'password'=>'required|min:8'
+      ]);
        //Attempt to login admin
-          if (Auth::attempt(['email' => $data['email'], 'password' => $data['password'], 'role' => '0'])) {
+          if (Auth::attempt(['email'=>$request->email,'password'=>$request->password],$request->remember, 'role' => '0'])) {
               //start admin session
-                Session::put('adminSession', $data['email']);
-                //Load the admin dashboard
-                return redirect('/admin')->with('flash_message_success', 'Login Successfull');
-            } else if (Auth::attempt(['email' => $data['email'], 'password' => $data['password'], 'role' => '1'])) {
-                Session::put('userSession', $data['email']);
-                return redirect('/user')->with('flash_message_success', 'Login Successfull');
+                Session::put('adminSession', $request->email);
+                //if successfull, redirect to intended location
+                return redirect()->intended(route('admin.dashboard')->with('flash_message_success', 'Login Successfull'));
+            } else if (Auth::attempt(['email'=>$request->email,'password'=>$request->password],$request->remember, 'role' => '1'])) {
+                Session::put('userSession', $request->email);
+                return redirect()->intended(route('user.dashboard')->with('flash_message_success', 'Login Successfull'));
             } else {
                 return redirect('/')->with('flash_message_error', 'Invalid Email Or password');
             }

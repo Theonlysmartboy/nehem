060e0f90b663b257f7577bcf63fe06149540daef
TheOnlySmartBoy <o2jose43@gmail.com>
2020-02-24 09:48:15 +0300

    added password reset and profile update functionality

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -38,5 +38,66 @@ class AdminController extends Controller
                 return redirect('/')->with('flash_message_error', 'Invalid Email Or password');
             }
 
+            public function settings() {
+        if (Session::has('adminSession')) {
+            return view('admin.settings');
+        } else {
+            return redirect('/admin')->with('flash_message_error', 'Access denied! Please Login first');
+        }
+    }
+
+    public function chkPassword(Request $request) {
+        if (Session::has('adminSession')) {
+            $data = $request->all();
+            $current_password = $data['current_pwd'];
+            $check_password = User::where(['role' => '1'])->first();
+            if (Hash::check($current_password, $check_password->password)) {
+                echo "true";
+                die;
+            } else {
+                echo "false";
+                die;
+            }
+        } else {
+            return redirect('/admin')->with('flash_message_error', 'Access denied! Please Login first');
+        }
+    }
+
+    public function updatePassword(Request $request) {
+        if (Session::has('adminSession')) {
+            if ($request->isMethod('post')) {
+                $data = $request->all();
+                $check_password = User::where(['email' => Auth::user()->email])->first();
+                $current_password = $data['current_pwd'];
+                $email = Auth::user()->email;
+                Hash::check($current_password, $check_password->password);
+                $password = bcrypt($data['new_pwd']);
+                User::where('email', $email)->update(['password' => $password]);
+                Session::flush();
+                return redirect('/')->with('flash_message_success', 'Password Updated Successfully');
+            }
+        } else {
+            return redirect('/')->with('flash_message_error', 'Access denied! Please Login first');
+        }
+    }
+
+    public function updateProfile(Request $request) {
+        if (Session::has('adminSession')) {
+            if ($request->isMethod('post')) {
+                $data = $request->all();
+                $email = Auth::user()->email;
+                User::where(['email' => $email])->update(['full_name' => $data['fname'], 'contact_adress' => $data['address'], 'telephone' => $data['tel']]);
+                return redirect('/admin/dashboard')->with('flash_message_success', 'Profile Updated Successfully');
+            } else {
+                return view('admin.profile');
+            }
+        } else {
+            return redirect('/')->with('flash_message_error', 'Access denied! Please Login first');
+        }
+    }
+
+    public function logout(){
+      Session::flush();
+      return redirect('/')->with('flash_message_success', 'Be blessed! See you gain later');
     }
 }

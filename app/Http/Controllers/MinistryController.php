f6c988864229e0037d0f707f204430c8b0a6c6d8
TheOnlySmartBoy <o2jose43@gmail.com>
2020-02-26 12:57:47 +0300

    Begun writing the minsitry module

--- app/Http/Controllers/MinistryController.php
+++ app/Http/Controllers/MinistryController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class MinistryController extends Controller
 {
-    //
+    public function index(){
+      if (Session::has('adminSession')) {
+
+    }else{
+      return redirect
+    }
+  }
 }

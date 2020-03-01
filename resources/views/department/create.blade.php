af82a91fec87e9b6a78c617a1496d3da89c8f6c6
TheOnlySmartBoy <o2jose43@gmail.com>
2020-03-01 20:46:47 +0300

    Added the department create view

---
+++ resources/views/department/create.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.adminlayout.admin_design')
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Ministry</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Home</a></li>
+              <li class="breadcrumb-item"><a href="{{route('view.ministry')}}">Ministries</a></li>
+              <li class="breadcrumb-item active">Edit</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-1">
+          </div>
+          <!--Center column-->
+          <div class="col-md-10">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Edit Ministry Details</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form role="form" action="{{url('ministry/edit/'.$ministryDetails->id)}}" method="post">@csrf
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" name="title" value="{{$ministryDetails->title}}">
+                  </div>
+                  <div class="form-group">
+                    <label for="desc">Description</label>
+                    <input type="text" class="form-control" id="desc" name="desc" value="{{$ministryDetails->desc}}">
+                  </div>
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer text-center">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+            </div>
+            <!-- /.col center-->
+             </div>
+             <div class="col-md-1">
+             </div>
+             <!-- /.row -->
+           </div>
+         <!--/.col (right) -->
+       </div>
+   </section>
+   <!-- /.content -->
+ </div>
+ <!-- /.content-wrapper -->
+@endsection
